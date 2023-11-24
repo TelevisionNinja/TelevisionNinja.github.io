e7314e9457dd44cecd021d5ee0a9af23e9f397f0
TelevisionNinja <TelevisionNinja@gmail.com>
2023-11-24 17:16:20 -0600

    removed input

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -62,59 +62,22 @@ export default function Contact() {
     return (
         <>
             <div className="container contact-page">
-                <div className="text-zone">
+                <div className="text-zone" align="center">
                     <h1>
                         {contactSpans}
                     </h1>
                     <p>
-                        I am interested in professional software development. If you have any other requests or questions, don't hesitate to contact me using the form below.
+                        I'm interested in professional software development. If you have any other requests or questions, don't hesitate to contact me.
                     </p>
-                    <div className="contact-form">
-                        <form ref={form} onSubmit={sendEmail}>
-                            <ul>
-                                <li className="half">
-                                    <input placeholder="Name" type="text" name="name" required/>
-                                </li>
-                                <li className="half">
-                                    <input
-                                        placeholder="Email"
-                                        type="email"
-                                        name="email"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <input
-                                        placeholder="Subject"
-                                        type="text"
-                                        name="subject"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <textarea
-                                        placeholder="Message"
-                                        name="message"
-                                        required
-                                    ></textarea>
-                                </li>
-                                <li>
-                                    <input type="submit" className="flat-button" value="SEND"/>
-                                </li>
-                            </ul>
-                        </form>
-                    </div>
-                </div>
-                <div className="info-map">
-                    Arkansas <br />
-                    <br />
-                    TelevisionNinja@gmail.com
+                    <h2>
+                        <a href="mailto:TelevisionNinja@gmail.com">TelevisionNinja@gmail.com</a>
+                    </h2>
                 </div>
                 <div className="map-wrap">
                     <MapContainer center={[34.74460280742035, -92.28834370644515]} zoom={5}>
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                         <Marker position={[34.74460280742035, -92.28834370644515]}>
-                            <Popup>Located here</Popup>
+                            <Popup>Arkansas</Popup>
                         </Marker>
                     </MapContainer>
                 </div>
